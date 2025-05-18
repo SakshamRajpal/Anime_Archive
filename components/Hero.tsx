156ecd72acfd08d978ad01f1934e424dd0f1ce3a
Saksham Rajpal <110560344+SakshamRajpal@users.noreply.github.com>
2025-05-18 14:06:45 +0530

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -10,6 +10,8 @@ function Hero() {
           width={101}
           height={96}
           className="object-contain"
+          style={{ width: 'auto', height: 'auto' }} // Added to fix aspect ratio warning
+          priority // Added for LCP optimization
         />
         <h1 className="sm:text-6xl text-5xl text-white lg:max-w-lg font-bold leading-[120%]">
           Explore The <span className="red-gradient">Diverse Realms</span> of
@@ -17,7 +19,13 @@ function Hero() {
         </h1>
       </div>
       <div className="lg:flex-1 relative w-full h-[50vh] justify-center">
-        <Image src="/anime.png" alt="anime" fill className="object-contain" />
+        <Image
+          src="/anime.png"
+          alt="anime"
+          fill
+          className="object-contain"
+          sizes="(max-width: 768px) 100vw, 50vw" // Added to fix performance warning
+        />
       </div>
     </header>
   );
